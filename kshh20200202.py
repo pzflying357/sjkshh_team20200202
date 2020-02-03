f86af09ca8d6f2fd531a7542b07548d48dda7502
pzflying357 <369284860@qq.com>
2020-02-03 17:17:21 +0800

    Create kshh20200202.py
    20200203 first commit

---
+++ kshh20200202.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+from pandas import Series
+
+f=open('D:/pystudy/数据可视化小组/task_1_zhaopin_data.csv',encoding='utf-8')
+df = pd.read_csv(f)
+#print(df1)
+f.close()
+df1=pd.DataFrame(df)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 300)
+print(df1)
+print(df1.head())
+print(df1.tail())
+print(df1.head(3))
+print(df1.iloc[2])
+
+print(df1.loc[:,['学历要求','工作经验','是否全职']])
+
+df2 = df.set_index("公司名")
+print(df2)
+
+df3=df2.drop(columns=["其他备注"])
+print(df3)
+
+df4=df3.drop('上海蜗兴科技有限公司')
+print(df4)
+df4.to_csv('D:/pystudy/数据可视化小组/task_1_zhaopin_data_update.csv')
+
+
+
+
